a9063f489ad57a1835ea736771152a40a4923ca5
telex1 <telexsound@gmail.com>
2022-07-13 16:20:21 -0700

    added frames

--- Byterun.py
+++ Byterun.py
@@ -22,7 +22,40 @@ class VirtualMachine(object):
 		
 		self.run_frame(frame)
 
+	# stuff for frames
+	def make_frame(self, code, callargs={}, global_names=None, local_names=None):
+		if global_names is not None and local_names is not None:
+			local_names = global_names
+		elif self.frames:
+			global_names = self.frame.global_names
+			local_names = {}
+		else:
+			global_names = local_names = {
+				'__builtins__': __builtins__,
+				'__name__': '__main__',
+				'__doc__': None,
+				'__package__': None,
+			}
+		local_names.update(callargs)
+		frame = Frame(code, global_names, local_names, self.frame)
+		return frame
+	
+	def push_frame(self, frame):
+		self.frames.append(frame)
+		self.frame = frame
+	
+	def pop_frame(self):
+		self.frames.pop()
+		if self.frames:
+			self.frame = self.frames[-1]
+		else:
+			self.frame = None
+	
+	def run_frame(self):
+		pass
 
+# each frame instance is associated with one code object and manages
+# the global and local namespaces, it also keeps a reference to the calling frame and the last bytecode instruction executed 
 class Frame(object):
 	
 	def __init__(self, code_obj, global_names, local_names, prev_frame):
@@ -39,6 +72,6 @@ class Frame(object):
 			if hasattr(self.builtin_names, '__dict__'):
 				self.builtin_names = self.builtin_names.__dict__
 
-		self.last_instruct = 0
+		self.last_instruction = 0
 		self.block_stack = []
 	
